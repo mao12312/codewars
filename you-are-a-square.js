5c86a922741e07d80e03ce65c3f6343ced264e99
mao12312 <42489486+mao12312@users.noreply.github.com>
2019-12-10 17:13:58 +0900

    Tweak

--- you-are-a-square.js
+++ you-are-a-square.js
@@ -4,18 +4,17 @@ var isSquare = function (n) {
     let sq = Math.sqrt(n);
     if (Number.isInteger(sq)) {
         res = `true, "${n} is a square number(${sq} * ${sq})"`;
-        return res;
-
+        console.log(res);
     } else if (Math.sign(n) === -1) {
         res = `false, "${n}: Negative numbers cannot be square numbers"`;
-        return res;
+        console.log(res);
     } else {
         res = `false, "${n} is not a square number"`;
-        return res;
+        console.log(res);
     }
 }
 // best answer
 // var isSquare = function (n) {
 //     console.log(Math.sqrt(n) % 1 === 0);
 // }
-isSquare(-1);
+isSquare(4);
