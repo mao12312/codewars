aad5cc2022237d481584000ad551500193b00038
mao12312 <42489486+mao12312@users.noreply.github.com>
2019-12-11 23:56:19 +0900

    add kata

--- counting-duplicates.js
+++ counting-duplicates.js
@@ -1,6 +1,21 @@
 // Counting Duplicates
+function duplicateCount(text) {
+    textSp = text.split("")
+    let count = 0;
+    let ex;
+    let res = [];
+    for (i = 0; i < textSp.length; i++) {
+        ex = new RegExp(textSp[i], 'ig')
 
-
+        if (text.match(ex).length > 1 && res.indexOf(textSp[i]) === -1) {
+            res.push(textSp[i]);
+            count++;
+        }
+    }
+    console.log(count);
+    console.log(res);
+}
+duplicateCount("aabBcde")
 
 
 
