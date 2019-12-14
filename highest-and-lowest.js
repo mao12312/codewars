b95ee6fff6da2ca6d790d4763fa83a4f1f41723c
mao12312 <42489486+mao12312@users.noreply.github.com>
2019-12-14 16:40:37 +0900

    Tweak

--- highest-and-lowest.js
+++ highest-and-lowest.js
@@ -1,6 +1,6 @@
 function highAndLow(numbers) {
-    a = numbers.split(" ").map(Number).sort((a, b) => a - b);
-    res = `${String(a.slice(-1)[0])} ${String(a[0])}`
+    spt = numbers.split(" ").map(Number).sort((a, b) => a - b);
+    res = `${String(spt.slice(-1)[0])} ${String(spt[0])}`
     console.log(res);
 }
 highAndLow("4 5 29 54 4 0 -214 542 -64 1 -3 6 -6")
