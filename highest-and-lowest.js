8a12abdb3447b07554cac7a72d39c2920ee872dc
mao12312 <42489486+mao12312@users.noreply.github.com>
2019-12-15 23:12:50 +0900

    Tweak

--- highest-and-lowest.js
+++ highest-and-lowest.js
@@ -1,6 +1,13 @@
+// My Answer
 function highAndLow(numbers) {
     spt = numbers.split(" ").map(Number).sort((a, b) => a - b);
     res = `${String(spt.slice(-1)[0])} ${String(spt[0])}`
     console.log(res);
+}
+
+// best  Answer
+function highAndLow(numbers) {
+    numbers = numbers.split(' ').map(Number);
+    return Math.max.apply(0, numbers) + ' ' + Math.min.apply(0, numbers);
 }
 highAndLow("4 5 29 54 4 0 -214 542 -64 1 -3 6 -6")
