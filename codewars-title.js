6c5a455ed016553aeb204725682301480a45d4ea
mao12312 <42489486+mao12312@users.noreply.github.com>
2020-01-01 23:24:31 +0900

    add kata

--- codewars-title.js
+++ codewars-title.js
@@ -1,4 +1,4 @@
 const title = (word) => {
     console.log("touch " + word.toLowerCase().split(' ').join('-') + ".js");
 }
-title("Is n divisible by x and y");
+title("Maximum subarray sum");
---
+++ maximum-subarray-sum.js
@@ -0,0 +1,4 @@
+var maxSequence = function (arr) {
+    // ...
+}
+maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4])
