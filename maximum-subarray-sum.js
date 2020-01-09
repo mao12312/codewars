25e134be7d62beddc22334034f542a84c03d9faf
mao12312 <42489486+mao12312@users.noreply.github.com>
2020-01-09 15:05:47 +0900

    add file-create

--- maximum-subarray-sum.js
+++ maximum-subarray-sum.js
@@ -9,21 +9,16 @@ var maxSequence = function (arr) {
         res = arr.slice(arr1[i], arr1[i+1] + 1)
         console.log(res)
     }
-
-
     console.log(arr1)
-
 }
-
-
 var maxSequence = function (arr) {
-    var max = 0;
-    var cur = 0;
-    arr.forEach(function (i) {
-        cur = Math.max(0, cur + i);
-        max = Math.max(max, cur);
-    });
-    return max;
+    var min = 0, ans = 0, i, sum = 0;
+    for (i = 0; i < arr.length; ++i) {
+        sum += arr[i];
+        min = Math.min(sum, min);
+        ans = Math.max(ans, sum - min);
+        console.log(i)
+    }
+    return ans;
 }
-
 maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4])
