26ba1dca85fd6aa93e25c6d025476c0069f2a83e
mao12312 <42489486+mao12312@users.noreply.github.com>
2020-01-17 14:57:50 +0900

    add kata

---
+++ moves-in-squared-strings(i).js
@@ -0,0 +1,13 @@
+function vertMirror(str) {
+    str = str.split('\n').map(function (e) { return e.split('').reverse().join('')}).join('\n');
+    return str
+}
+function horMirror(str) {
+    str = str.split('\n').reverse().join('\n');
+    return str
+}
+function oper(fct, s) {
+    return fct(s);
+}
+oper(vertMirror, "hSgdHQ\nHnDMao\nClNNxX\niRvxxH\nbqTVvA\nwvSyRu")
+oper(horMirror, "njMK\ndbrZ\nLPKo\ncEYz")
