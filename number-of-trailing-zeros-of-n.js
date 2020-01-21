e1fae17e4fe14075fd4de0015c68a8da68d5cabd
mao12312 <42489486+mao12312@users.noreply.github.com>
2020-01-21 10:35:43 +0900

    Tweak

--- number-of-trailing-zeros-of-n.js
+++ number-of-trailing-zeros-of-n.js
@@ -1,20 +1,30 @@
+// const zeros = (n) => {
+//     let num = 1;
+//     let count = 0;
+//     if (n === 0) { return 0 }
+//     for (let i = 1; i <= n; i++) {
+//         console.log(num *= i);
+//     }
+//     let res = String(num).split('')
+//     for (let j = res.length - 1; j >= 0; j--) {
+//         if(res[j]==="0"){
+//             count++
+//         }else{
+//             console.log(count)
+//             break
+//         }
+//     }
+//     console.log(res)
+//     console.log(num)
+// }
+
 const zeros = (n) => {
-    let num = 1;
     let count = 0;
-    if (n === 0) { return 0 }
-    for (let i = 1; i <= n; i++) {
-        console.log(num *= i);
+    let i = 5;
+    while (n / i >= 1) {
+        count += Math.floor(n / i);
+        i *= 5;
     }
-    let res = String(num).split('')
-    // for (let j = res.length - 1; j >= 0; j--) {
-    //     if(res[j]==="0"){
-    //         count++
-    //     }else{
-    //         console.log(count)
-    //         break
-    //     }
-    // }
-    // console.log(res)
-    console.log(num)
+    return count
 }
-zeros(30)
+zeros(100)
