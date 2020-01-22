ebb4cc388f3d80f8385a510b647db158f2f4d16d
mao12312 <42489486+mao12312@users.noreply.github.com>
2020-01-22 18:03:19 +0900

    add kata

--- grasshopper-grade-book.js
+++ grasshopper-grade-book.js
@@ -1,3 +1,25 @@
 function getGrade(s1, s2, s3) {
-    // Code here
+    let score = (s1 + s2 + s3) / 3;
+    if (90 <= score && score <= 100) {
+        return 'A';
+    } else if (80 <= score && score < 90) {
+        return 'B';
+    } else if (70 <= score && score < 80) {
+        return 'C';
+    } else if (60 <= score && score < 70) {
+        return 'D';
+    } else if (0 <= score && score < 60) {
+        return 'F';
+    }
+}
+
+getGrade(70, 70, 100)
+
+function getGrade(s1, s2, s3) {
+    avg = (s1 + s2 + s3) / 3;
+    if (avg < 60) return "F";
+    else if (avg < 70) return "D";
+    else if (avg < 80) return "C";
+    else if (avg < 90) return "B";
+    else return "A";
 }
