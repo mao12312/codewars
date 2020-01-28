414dc0fa349a51232ae9f94d933dcad1608b2241
mao12312 <42489486+mao12312@users.noreply.github.com>
2020-01-28 19:25:06 +0900

    Tweak

--- primorial-of-a-number.js
+++ primorial-of-a-number.js
@@ -1,9 +1,19 @@
 const numPrimorial = (n) => {
     let prime = [];
-    // for (i = 2; prime.length < n; i++) {
-    //     if (i === 2) prime.push(i)
-    // }
-    
-    console.log(prime)
+    for (i = 2; prime.length < n; i++) {
+        if (primeNumber(i)) {
+            prime.push(i)
+        }
+    }
+    let res = prime.reduce((prev, cur) => {return prev * cur });
+    return res;
 }
-numPrimorial(5)
+const primeNumber = (i) => {
+    if (i === 2) return true;
+    for (let j = 2; j < i; j++) {
+        if (i % j === 0) return false;
+    }
+    return true;
+}
+numPrimorial(3)
+
