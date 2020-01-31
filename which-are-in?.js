ddb9e5dc073e9c264e82b970df3baa2c9d85ecbb
mao12312 <42489486+mao12312@users.noreply.github.com>
2020-01-31 14:54:43 +0900

    add kata

--- which-are-in?.js
+++ which-are-in?.js
@@ -1,10 +1,16 @@
-const inArray = (ar1,ar2) => {
-    for(let i = 0;i<ar2.length;i++){
-        for(let j = 0; j<ar1.length;j++){
-            let re = ar1[i]
+const inArray = (ar1, ar2) => {
+    let res = [];
+    for (let i = 0; i < ar2.length; i++) {
+        for (let j = 0; j < ar1.length; j++) {
+            let re = new RegExp(ar1[j] + '(.*?)', 'g')
+            if (ar2[i].match(re)) {
+                res.push(ar1[j])
+            }
         }
     }
+    let setRes = new Set(res)
+    return Array.from(setRes).sort()
 }
 a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
-a1 = ["xyz", "live", "strong"]
+a1 = ["live", "strong", "arp"]
 inArray(a1, a2)
