13d6293175ddba4ffb5a83a7f38563ba491db849
mao12312 <42489486+mao12312@users.noreply.github.com>
2020-02-08 15:30:44 +0900

    Tweak

--- sum-mixed-array.js
+++ sum-mixed-array.js
@@ -1,14 +1,15 @@
-// const sumMix = (x) => {
-//     let res = 0;
-//     for (let i = 0; i < x.length; i++) {
-//         if (typeof (x[i]) === "string") {
-//             x.splice(i, 1, Number(x[i]))
-//         }
-//         res += x[i]
-//     }
-//     console.log(res)
-// }
+const sumMix = (x) => {
+    let res = 0;
+    for (let i = 0; i < x.length; i++) {
+        if (typeof (x[i]) === "string") {
+            x.splice(i, 1, Number(x[i]))
+        }
+        res += x[i]
+    }
+    console.log(res)
+}
 
+// Another answer
 const sumMix = (x) => {
     let intX = x.map((e) => { return typeof (e) === "string" ? Number(e) : e })
     return intX.reduce((pre, cur) => { return pre + cur })
