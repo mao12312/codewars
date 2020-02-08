166c7198ff1b2f663605086f472e8b7489d662fb
mao12312 <42489486+mao12312@users.noreply.github.com>
2020-02-08 14:09:51 +0900

    add kata

--- sum-mixed-array.js
+++ sum-mixed-array.js
@@ -1,12 +1,16 @@
+// const sumMix = (x) => {
+//     let res = 0;
+//     for (let i = 0; i < x.length; i++) {
+//         if (typeof (x[i]) === "string") {
+//             x.splice(i, 1, Number(x[i]))
+//         }
+//         res += x[i]
+//     }
+//     console.log(res)
+// }
+
 const sumMix = (x) => {
-    let res, i = 0;
-    for (i; i < x.length; i++) {
-        console.log(typeof(x[i]))
-        if (typeof(x[i]) === String) {
-            Number(x[i]);
-        }
-        res+=x[i]
-    }
-    console.log(res)
+    let intX = x.map((e) => { return typeof (e) === "string" ? Number(e) : e })
+    return intX.reduce((pre, cur) => { return pre + cur })
 }
 sumMix([9, 3, '7', '3'])
