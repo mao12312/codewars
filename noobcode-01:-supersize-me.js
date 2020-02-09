d4fc97d21d9971865484317d0bc4c1fa7ea94393
mao12312 <42489486+mao12312@users.noreply.github.com>
2020-02-09 22:45:56 +0900

    Tweak

--- noobcode-01:-supersize-me.js
+++ noobcode-01:-supersize-me.js
@@ -3,4 +3,11 @@ const superSize = (num) => {
     let res = parseInt(numSpt.sort((a, b) => b - a).join(''));
     return res;
 }
+
+const superSize = (num) => {
+    let numSpt = String(num).split('').map(str => parseInt(str, 10));
+    let res = parseInt(numSpt.sort().reverse().join(''));
+    console.log(res)
+}
+
 superSize(2017)
