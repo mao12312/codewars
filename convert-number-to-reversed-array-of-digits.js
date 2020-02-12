edd3a528c26933aab5b3e9ce5f733fd5e4a20441
mao12312 <42489486+mao12312@users.noreply.github.com>
2020-02-12 16:28:34 +0900

    add kata

--- convert-number-to-reversed-array-of-digits.js
+++ convert-number-to-reversed-array-of-digits.js
@@ -2,4 +2,9 @@ const digitize = (n) => {
     let res = String(n).split('').reverse().map(num => { return Number(num) })
     return res
 }
+// other answer
+const digitize = (n) => {
+    let res = (n+"").split('').map(Number).reverse()
+    return res
+}
 digitize(35231)
