a3af05640f3d6eb5a108ef09d1f167e3cc851a39
mao12312 <42489486+mao12312@users.noreply.github.com>
2020-02-23 23:35:51 +0900

    add kata

--- file-create.js
+++ file-create.js
@@ -9,4 +9,4 @@ const title = (word) => {
         else console.log('create end');
     });
 }
-title("Square Every Digit");
+title("String ends with?");
---
+++ string-ends-with?.js
@@ -0,0 +1,12 @@
+const solution = (str, ending) => {
+    let res = str.endsWith(ending)
+    return res;
+}
+
+
+const solution = (str, ending) => {
+    let reg = new RegExp(ending + '$', 'i')
+    let res = reg.test(str)
+    return res;
+}
+solution('abcde', 'cde')
