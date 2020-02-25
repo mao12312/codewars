86e0c33bb6f265a92569d889f077a453331239cd
mao12312 <42489486+mao12312@users.noreply.github.com>
2020-02-25 23:55:50 +0900

    add kata

--- growth-of-a-population.js
+++ growth-of-a-population.js
@@ -1,4 +1,8 @@
 const nbYear = (p0, percent, aug, p) => {
-
+    let age = 0
+    for (i = p0; i < p; age++) {
+        i = i + i * (percent / 100) + aug
+    }
+    return age
 }
 nbYear(1500, 5, 100, 5000)
