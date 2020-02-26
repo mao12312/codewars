bd7e36bf282ab3e0dd7faf548fca8fe7441b5fbd
mao12312 <42489486+mao12312@users.noreply.github.com>
2020-02-26 23:28:26 +0900

    Tweak

--- string-ends-with?.js
+++ string-ends-with?.js
@@ -1,11 +1,11 @@
 const solution = (str, ending) => {
-    let res = str.endsWith(ending)
+    let res = str.endsWith(ending);
     return res;
 }
 
 const solution = (str, ending) => {
-    let reg = new RegExp(ending + '$', 'i')
-    let res = reg.test(str)
+    let reg = new RegExp(ending + '$', 'i');
+    let res = reg.test(str);
     return res;
 }
 solution('abcde', 'cde')
