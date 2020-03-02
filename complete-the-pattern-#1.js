8a1e09fc7849245add6c6d032cd453a1cb2aa993
mao12312 <42489486+mao12312@users.noreply.github.com>
2020-03-03 00:10:31 +0900

    Tweak

--- complete-the-pattern-#1.js
+++ complete-the-pattern-#1.js
@@ -1,10 +1,13 @@
 const pattern = (n) => {
     let output = "";
     for (i = 1; i <= n; i++) {
-        for(j=1;j<=i;j++){
-            
+        for (j = 0; j < i; j++) {
+            output += i
+        }
+        if (i === n) break; {
+            output += "\n"
         }
     }
-    console.log(output)
+    return output
 }
 pattern(2)
