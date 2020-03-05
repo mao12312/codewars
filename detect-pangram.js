7e655c65b2790b57df3fe184e0e654ccbec0cd95
mao12312 <42489486+mao12312@users.noreply.github.com>
2020-03-05 23:36:39 +0900

    add kata

--- detect-pangram.js
+++ detect-pangram.js
@@ -1,4 +1,13 @@
 const isPangram = (string) => {
-
+    let alphabet = "abcdefghijklmnopqrstuvxyz";
+    let res = string.toLowerCase().split('').map(e => { return alphabet = alphabet.replace(e, "") }).pop()
+    return res.length === 0 ? true : false;
 }
-isPangram("The quick brown fox jumps over the lazy dog.")
+isPangram("The quick barown fox jumps over the lazy dog.")
+
+// function isPangram(string) {
+//     string = string.toLowerCase();
+//     return "abcdefghijklmnopqrstuvwxyz".split("").every(function (x) {
+//         return string.indexOf(x) !== -1;
+//     });
+// }
