966d5b8573854d93363e8899205075ac6061ec47
mao12312 <42489486+mao12312@users.noreply.github.com>
2020-03-07 19:23:44 +0900

    Tweak

--- fibonatti.js
+++ fibonatti.js
@@ -11,3 +11,21 @@ let length = 40;
 for (let i = 0; i <= length; i++) {
     console.log(fib(i))
 }
+
+// user Map
+'use strict'
+const memo = new Map();
+memo.set(0, 0);
+memo.set(1, 1);
+const fib = (n) => {
+    if (memo.has(n)) {
+        return memo.get(n)
+    }
+    const val = fib(n - 1) + fib(n - 2);
+    memo.set(n, val);
+    return val;
+}
+let length = 40;
+for (let i = 0; i <= length; i++) {
+    console.log(fib(i))
+}
