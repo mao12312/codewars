5967e7f85ed3f03ae717836a60eee73e382b871d
mao12312 <42489486+mao12312@users.noreply.github.com>
2020-03-25 00:20:56 +0900

    Tweak

--- simple-fun-#74:-growing-plant.js
+++ simple-fun-#74:-growing-plant.js
@@ -11,8 +11,17 @@ const growingPlant = (upSpeed, downSpeed, desiredHeight) => {
             len -= downSpeed;
             count++
         }
-
     }
     return count
+}
+
+const growingPlant = (upSpeed, downSpeed, desiredHeight) => {
+    if (desiredHeight - upSpeed < 0) {
+        return 1
+    } else {
+        // （目標値 - 1日あたりの増加量）/（1日あたりの増加量 - 1日あたりの減少量）
+        return Math.ceil((desiredHeight - upSpeed) / (upSpeed - downSpeed)) + 1
+    };
+
 }
 growingPlant(10, 9, 4)
