8117473a26163d1c5643371b4406062e4e83274e
mao12312 <42489486+mao12312@users.noreply.github.com>
2020-03-25 00:03:10 +0900

    add kata

--- simple-fun-#74:-growing-plant.js
+++ simple-fun-#74:-growing-plant.js
@@ -1,4 +1,18 @@
 const growingPlant = (upSpeed, downSpeed, desiredHeight) => {
+    let len = 0;
+    let count = 0
 
+    while (len < desiredHeight) {
+        len += upSpeed;
+        if (len >= desiredHeight) {
+            count++
+            break;
+        } else {
+            len -= downSpeed;
+            count++
+        }
+
+    }
+    return count
 }
-growingPlant(100, 10, 910)
+growingPlant(10, 9, 4)
