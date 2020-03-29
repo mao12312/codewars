3719824dda5ddbd8636edd683abb7506938649d1
mao12312 <42489486+mao12312@users.noreply.github.com>
2020-03-29 23:52:18 +0900

    add kata

--- simple-assembler-interpreter.js
+++ simple-assembler-interpreter.js
@@ -1,5 +1,20 @@
 const simple_assembler = (program) => {
-    
+    const mem = {}
+    for (let i = 0; i < program.length; ++i) {
+        const [op, reg, val] = program[i].split(" ")
+        if (op === "mov") {
+            mem[reg] = isNaN(val) ? mem[val] : +val
+        } else if (op === "inc") {
+            mem[reg] = (mem[reg] || 0) + 1
+        } else if (op === "dec") {
+            mem[reg] = (mem[reg] || 0) - 1
+        } else if (op === "jnz") {
+            if (mem[reg] !== 0) {
+                i += val - 1
+            }
+        }
+    }
+    return mem
 }
 simple_assembler(['mov a 5', 'inc a', 'dec a', 'dec a', 'jnz a -1', 'inc a'])
 // 1
